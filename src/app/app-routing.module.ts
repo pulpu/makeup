d4404ab0278c0c1315e70e55bd9c6419c99d9517
Pulpu <gabriel.cosuta@gmail.com>
2018-08-09 15:18:19 -0400

    APS-004 add markup and router, anf fix font path

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { LanguageComponent } from './language/language.component';
+import { NavComponent } from './language/nav/nav.component';
+import { ParagraphComponent } from './language/nav/paragraph/paragraph.component';
+import { AdminComponent } from './language/nav/admin/admin.component';
+import { GalleryComponent } from './language/nav/gallery/gallery.component';
+import { IntroComponent } from './language/nav/intro/intro.component';
+//import { ThemeVideoComponent } from './language/nav/theme-video/theme-video.component';
+import { HomeComponent } from './language/home/home.component';
+import { Error404Component } from './language/error404/error404.component';
+
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent, children: [
+    { path: ':language', component: IntroComponent },
+    { path: ':language/:category', component: GalleryComponent }, 
+  ]}, 
+
+   {path: 'admin/ro/:id', component: AdminComponent, data: {message: 'Admin Page'}},
+
+    {path: '', redirectTo: '/home/ro', pathMatch: 'full'},
+    // {path: '404', component: PageNotFoundComponent},
+    {path: '404', component: Error404Component, data: {message: 'Page not found!'}},
+    {path: '**', redirectTo: '404'}
+];
+
+
+@NgModule({
+imports: [
+	RouterModule.forRoot( appRoutes )
+], 
+exports: [ RouterModule]
+})
+
+export class AppRouterModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { LanguageComponent } from './language/language.component';
+import { HomeComponent } from './language/home/home.component';
+import { NavComponent } from './language/nav/nav.component';
+import { IntroComponent } from './language/nav/intro/intro.component';
+import { GalleryComponent } from './language/nav/gallery/gallery.component';
+import { Error404Component } from './language/error404/error404.component';
+import { AdminComponent } from './language/nav/admin/admin.component';
+import { ParagraphComponent } from './language/nav/paragraph/paragraph.component';
+import { AppRouterModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LanguageComponent,
+    HomeComponent,
+    NavComponent,
+    IntroComponent,
+    GalleryComponent,
+    Error404Component,
+    AdminComponent,
+    ParagraphComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule,
+    HttpModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
